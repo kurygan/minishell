f23600d5e8840e272adec20b8ce3657e41b08a47
Bayarkette <nosens010101@gmail.com>
2025-05-20 23:50:02 +0200

    update to mkettab

--- src/parsing.c
+++ src/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emetel <emetel@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*   By: mkettab <mkettab@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:01:33 by mkettab           #+#    #+#             */
-/*   Updated: 2025/05/20 23:46:10 by emetel           ###   ########.fr       */
+/*   Updated: 2025/05/20 23:06:11 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,12 @@
 
 t_type	*handle_line(char *line, char **env)
 {
-	char	*path_env;
-	char	**paths;
-	char	*full;
-	int		i;
-
-	path_env = getenv("PATH");
-	paths = ft_split(path_env, ':');
-	if (!paths)
-		return (NULL);
-	i = 0;
-	while (paths[i])
-	{
-		full = ft_strjoin(paths[i], "/");
-		full = ft_strjoin(full, cmd);
-		if (access(full, X_OK) == 0)
-		{
-			free_argv(paths);
-			return (full);
-		}
-		free(full);
-		i++;
-	}
-	free_argv(paths);
-	return (NULL);
-}
-
-void	handle_line(char *line, char **av, char **env)
-{
-	pid_t	pid;
-	char	*path;
-
-	path = NULL;
-	printf("LIGNE=%s\n", line);
-	if (!av[0])
-	{
-		free_argv(av);
-		return ;
-	}
-	pid = fork();
-	if (pid == 0)
+	t_type	*command;
+	t_type	*temp;
+	
+	command = ft_calloc(1, sizeof(t_type));
+	temp = command;
+	if (*line == '<')
 	{
 		if (line[1] == '<')
 			command->token = HEREDOC;
