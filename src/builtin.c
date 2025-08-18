ae8a7cf34efc5a4471ecebfbb4175b28d234e5a2
Emilie Metel <emetel@k1r4p15.42mulhouse.fr>
2025-08-18 02:25:08 +0200

    echo gerer

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: emetel <emetel@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 01:46:05 by mkettab           #+#    #+#             */
-/*   Updated: 2025/08/14 18:50:02 by emetel           ###   ########.fr       */
+/*   Updated: 2025/08/18 01:20:11 by emetel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,34 @@ bool	is_builtin(char	*cmd)
 	return (false);
 }
 
+static bool	is_n_option(char *str)
+{
+	int	i;
+
+	if (!str || str[0] != '-')
+		return (false);
+	i = 1;
+	while (str[i] && str[i] == 'n')
+		i++;
+	return (str[i] == '\0');
+}
+
 void	exec_echo(t_cmd_segment *cmd)
 {
 	char	**args;
 	bool	print;
+	bool	no_newline;
 
 	if (!cmd->args)
 		return ((void)ft_putstr_fd("\n", 1));
 	args = cmd->args;
+	no_newline = false;
 	print = false;
+	while (*args && is_n_option(*args))
+	{
+		no_newline = true;
+		args++;
+	}
 	while (*args)
 	{
 		if (print)
@@ -39,7 +58,8 @@ void	exec_echo(t_cmd_segment *cmd)
 		ft_putstr_fd(*args, 1);
 		args++;
 	}
-	ft_putstr_fd("\n", 1);
+	if (!no_newline)
+		ft_putstr_fd("\n", 1);
 }
 
 void	exec_builtin(t_cmd_segment *cmd)
