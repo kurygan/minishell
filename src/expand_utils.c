aa2a66f902066864686529470e7d622b44db4168
Emilie Metel <emetel@k1r4p13.42mulhouse.fr>
2025-08-24 22:19:02 +0200

    multiple expand working

--- src/expand_utils.c
+++ src/expand_utils.c
@@ -6,7 +6,7 @@
 /*   By: emetel <emetel@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 18:00:00 by emetel            #+#    #+#             */
-/*   Updated: 2025/08/24 21:42:23 by emetel           ###   ########.fr       */
+/*   Updated: 2025/08/24 22:15:51 by emetel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,49 @@
 
 char	*expand_var(char *arg, t_sys *sys, int exit_status)
 {
+	char	*result;
 	char	*var_name;
 	char	*value;
+	int		i;
+	int		start;
 
 	if (!arg || arg[0] != '$')
 		return (gc_strdup(arg, &(sys->garbage)));
-	if (arg[1] == '?')
-		return (gc_itoa(exit_status, &(sys->garbage)));
-	var_name = arg + 1;
-	value = get_env_value_from_list(var_name, sys->env_list);
-	if (value)
-		return (gc_strdup(value, &(sys->garbage)));
-	return (gc_strdup("", &(sys->garbage)));
+	
+	result = gc_strdup("", &(sys->garbage));
+	i = 0;
+	
+	while (arg[i])
+	{
+		if (arg[i] == '$')
+		{
+			if (arg[i + 1] == '?')
+			{
+				value = gc_itoa(exit_status, &(sys->garbage));
+				result = gc_strjoin(result, value, &(sys->garbage));
+				i += 2;
+			}
+			else
+			{
+				start = i + 1;
+				while (arg[start] && (ft_isalnum(arg[start]) || arg[start] == '_'))
+					start++;
+				var_name = gc_substr(arg, i + 1, start - i - 1, &(sys->garbage));
+				value = get_env_value_from_list(var_name, sys->env_list);
+				if (value)
+					result = gc_strjoin(result, gc_strdup(value, &(sys->garbage)), &(sys->garbage));
+				gc_free(var_name, &(sys->garbage));
+				i = start;
+			}
+		}
+		else
+		{
+			char *temp = gc_substr(arg, i, 1, &(sys->garbage));
+			result = gc_strjoin(result, temp, &(sys->garbage));
+			i++;
+		}
+	}
+	return (result);
 }
 
 void	expand_quoted_str(char **str, t_sys *sys, int exit_status,
