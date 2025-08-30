cfcc9a0220567fe10e55b0b7a6fa69860923e819
Emilie Metel <emetel@k1r4p5.42mulhouse.fr>
2025-08-30 12:34:34 +0200

    318 ok

--- src/expander.c
+++ src/expander.c
@@ -6,22 +6,106 @@
 /*   By: emetel <emetel@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 01:50:01 by emetel            #+#    #+#             */
-/*   Updated: 2025/08/30 11:53:49 by emetel           ###   ########.fr       */
+/*   Updated: 2025/08/30 12:33:38 by emetel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+static char	*expand_mixed_quotes(char *str, t_sys *sys, int exit_status)
+{
+	int		i;
+	char	*result;
+	char	*temp;
+	char	*part;
+	int		start;
+
+	if (!str)
+		return (NULL);
+	result = gc_strdup("", &(sys->garbage));
+	i = 0;
+	while (str[i])
+	{
+		start = i;
+		if (str[i] == '\'' || str[i] == '\"')
+		{
+			char quote = str[i];
+			i++;
+			while (str[i] && str[i] != quote)
+				i++;
+			if (str[i])
+				i++;
+			part = gc_substr(str, start, i - start, &(sys->garbage));
+			temp = process_quoted_arg(part, sys, exit_status);
+			result = gc_strjoin(result, temp, &(sys->garbage));
+		}
+		else
+		{
+			while (str[i] && str[i] != '\'' && str[i] != '\"')
+				i++;
+			if (i > start)
+			{
+				part = gc_substr(str, start, i - start, &(sys->garbage));
+				if (part[0] == '$')
+					expand_variable_str(&part, sys, exit_status);
+				else if (part[0] == '~')
+					part = expand_tilde(part, sys);
+				result = gc_strjoin(result, part, &(sys->garbage));
+			}
+		}
+	}
+	return (result);
+}
+
 static void	expand_single_str(char **str, t_sys *sys, int exit_status)
 {
 	char	*original;
 	size_t	len;
+	int		has_mixed_quotes;
+	int		i;
 
 	if (!*str)
 		return ;
 	original = *str;
 	len = ft_strlen(original);
-	if (original[0] == '\'' && original[len - 1] == '\'')
+	
+	// Check if string contains mixed quotes or quotes with adjacent text
+	has_mixed_quotes = 0;
+	i = 0;
+	while (original[i])
+	{
+		if (original[i] == '\'' || original[i] == '\"')
+		{
+			char quote = original[i];
+			i++;
+			while (original[i] && original[i] != quote)
+				i++;
+			if (original[i])
+				i++;
+			// If there's text after the closing quote, it's mixed
+			if (original[i] && original[i] != ' ' && original[i] != '\t')
+			{
+				has_mixed_quotes = 1;
+				break;
+			}
+		}
+		else
+		{
+			// If we find text before quotes, it's mixed
+			int start_text = i;
+			while (original[i] && original[i] != '\'' && original[i] != '\"')
+				i++;
+			if (original[i] && i > start_text)
+			{
+				has_mixed_quotes = 1;
+				break;
+			}
+		}
+	}
+	
+	if (has_mixed_quotes)
+		*str = expand_mixed_quotes(original, sys, exit_status);
+	else if (original[0] == '\'' && original[len - 1] == '\'')
 		*str = process_quoted_arg(original, sys, exit_status);
 	else if (original[0] == '\"' && original[len - 1] == '\"')
 		*str = process_quoted_arg(original, sys, exit_status);
