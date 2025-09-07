e849d2dccc5e1c4bf8acbe46f18fb4491b73807f
Emilie Metel <emetel@k2r1p10.42mulhouse.fr>
2025-09-07 18:21:06 +0200

    ctrl Z gere

--- src/signal.c
+++ src/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkettab <mkettab@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*   By: emetel <emetel@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 01:51:32 by emetel            #+#    #+#             */
-/*   Updated: 2025/08/30 14:39:40 by mkettab          ###   ########.fr       */
+/*   Updated: 2025/09/07 18:20:43 by emetel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	setup_interactive_signals(void)
 {
 	signal(SIGINT, sigint_handler);
 	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
 }
 
 void	setup_exec_signals(void)
